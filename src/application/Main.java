3b1c7635af10e341b7f4453d99993e8ac565efaf
Leonard Asp <Leoasp96@gmail.com>
2016-05-20 15:51:01 +0200

    KNOT

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,7 +7,7 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		System.out.println("Hello World leo suger");
+		System.out.println("Hello World leo suger KNOT");
 	
 		
 	}
