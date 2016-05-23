616e382189494ab507071c5adb6e12da3bb10d57
Leonard Asp <Leoasp96@gmail.com>
2016-05-23 10:29:38 +0200

    Platform

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,13 +3,30 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application implements Runnable {
 
+	boolean running = true;
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		System.out.println("Hello World leo suger KNOT");
+		Main main = new Main();
+		Thread thread = new Thread(main);
+		thread.start();
 	
-		
+
+	}
+
+	public void run() {
+		while (running) {
+			try {
+				Thread.sleep(17);
+				System.out.println("Running");
+			} catch (InterruptedException e) {
+
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 }
---
+++ src/application/Platform.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javafx.scene.shape.*;
+import javafx.scene.shape.Shape;
+
+public class Platform {
+	private List<Shape> rectangle = new ArrayList<Shape>();
+	private double width = 300;
+	private double height = 20;
+	private double startX;
+	private double startY;
+	private Line north;
+	private Line east;
+	private Line south;
+	private Line west;
+
+	/** Enter the startX and startY of the platform **/
+	public Platform(double startX, double startY) {
+		this.startX = startX;
+		this.startY = startY;
+
+		north = new Line(startX, startY, startX + width, startY);
+
+		east = new Line(north.getEndX(), north.getEndY(), north.getEndX(), north.getEndY() + height);
+
+		south = new Line(east.getEndX(), east.getEndY(), east.getEndX() - width, east.getEndY());
+
+		west = new Line(startX, startY, startX, startY + height);
+
+		rectangle.addAll(Arrays.asList(north, west, south, east));
+
+	}
+
+	public List<Shape> getPlatform() {
+		return rectangle;
+	}
+
+}
