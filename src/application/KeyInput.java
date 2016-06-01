31ab3bca6900f737ccc0a6ade3ba7b2c931464d1
Leonard Asp <Leoasp96@gmail.com>
2016-06-01 10:37:44 +0200

    Comments i Keyinput och Scoreboard

--- src/application/KeyInput.java
+++ src/application/KeyInput.java
@@ -1,109 +1,88 @@
 package application;
 
-import javafx.event.ActionEvent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
+/** Keyinput adds keyEventsHandlers to the stage **/
 public class KeyInput {
 	private String message;
-    private boolean isShowing = false;
-
+	private boolean isShowing = false;
+	
+	/** Constructor demands a reference to the "controller" **/ //which allows the class to work with other classes
 	public KeyInput(Bank bank) {
 
 		bank.getLogin().getStage().addEventHandler(KeyEvent.KEY_PRESSED, ep -> {
+			//sets the players velocity to "left" and changes the image to the left-looking one
 			if (ep.getCode() == KeyCode.LEFT) {
 				bank.getMove().moveLeft();
 				bank.getGui().getPlayerCircle().setFill(bank.getGui().getPatternLookLeft());
 				bank.getPlayer().setLeft(true);
 			}
+			//Shoots the bullet
 			if (ep.getCode() == KeyCode.SPACE) {
 				bank.getBullet().shootBullet();
 			}
+			//sets the players velocity to "right" and changes the image to the right-looking one
 			if (ep.getCode() == KeyCode.RIGHT) {
 				bank.getMove().moveRight();
 				bank.getGui().getPlayerCircle().setFill(bank.getGui().getPatternLookRight());
 				bank.getPlayer().setLeft(false);
 			}
+			//Player jumps
 			if (ep.getCode() == KeyCode.UP) {
 				bank.getMove().jump();
 			}
-
-			if(ep.getCode() == KeyCode.TAB){
+			//Shows the scoreboard panel
+			if (ep.getCode() == KeyCode.TAB) {
 				bank.getGui().getScorePanel().setStyle("visibility: visible");
 			}
 
 		});
 
-		bank.getLogin().getStage().addEventHandler(KeyEvent.KEY_PRESSED, ep -> {
-
-
-		});
-
 		bank.getLogin().getStage().addEventHandler(KeyEvent.KEY_RELEASED, er -> {
+			//Removes the players right velocity
 			if (er.getCode() == KeyCode.RIGHT) {
 				bank.getMove().resetVelRight();
 			}
+			//Removes the players left velocity
 			if (er.getCode() == KeyCode.LEFT) {
 				bank.getMove().resetVelLeft();
 			}
-			if(er.getCode() == KeyCode.TAB){
+			//Hides the scoreboard panel
+			if (er.getCode() == KeyCode.TAB) {
 				bank.getGui().getScorePanel().setStyle("visibility: hidden");
 			}
 		});
 
-		bank.getLogin().getStage().addEventHandler(KeyEvent.KEY_RELEASED, er -> {
-			
-		});
-
-		bank.getLogin().getStage().addEventHandler(KeyEvent.KEY_PRESSED, ej -> {
-			
-			bank.getLogin().getStage().addEventHandler(KeyEvent.KEY_PRESSED, es -> {
-				
-			});
-		});
-
-
-		bank.getGui().getMessageInput().addEventHandler(ActionEvent.ANY , e -> {
-
-
-		});
-
-        bank.getLogin().getStage().addEventHandler(KeyEvent.KEY_PRESSED , event -> {
-            if(event.getCode() == KeyCode.ENTER){
-
-				if(isShowing){
+		bank.getLogin().getStage().addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+			//Enables/disables the chatt when pressing ENTER
+			if (event.getCode() == KeyCode.ENTER) {
+				if (isShowing) {
 					message = bank.getGui().getMessageInput().getText();
-					if(!message.equals("")){
+					if (!message.equals("")) {
 						bank.getPlayer().setMessage(message);
 						bank.getGui().getMessageInput().clear();
 						bank.getGui().getChatLog().setFocusTraversable(false);
 						bank.getGui().getMessageInput().setFocusTraversable(false);
 						isShowing = false;
 					}
-                    bank.getGui().getChatUI().setStyle("visibility: hidden");
+					bank.getGui().getChatUI().setStyle("visibility: hidden");
 					bank.getGui().getMessageInput().setStyle("visibility: hidden");
 					bank.getGui().getChatLog().setStyle("-fx-background-color: rgba(59, 64, 68, 0.2);");
 					bank.getGui().getChatLog().lookup(".scroll-bar:vertical").setStyle("-fx-opacity: 0");
 					isShowing = false;
-				}else{
+				} else {
 					bank.getGui().getMessageInput().setFocusTraversable(true);
-                    bank.getGui().getChatUI().setStyle("visibility: visible");
+					bank.getGui().getChatUI().setStyle("visibility: visible");
 					bank.getGui().getMessageInput().setStyle("visibility: visible");
 
 					bank.getGui().getChatLog().setStyle("-fx-background-color: rgba(59, 64, 68, 0.4);");
 					bank.getGui().getChatLog().lookup(".scroll-bar:vertical").setStyle("-fx-opacity: 1");
 					isShowing = true;
 				}
-            }
+			}
 
-
-        });
-
-
-
-
-
-
+		});
 
 	}
 
--- src/application/Scoreboard.java
+++ src/application/Scoreboard.java
@@ -1,10 +1,10 @@
 package application;
-
+/** Scoreboard class updates the scoeboard 2.5 times a second  **/
 public class Scoreboard implements Runnable {
 
 	private Bank bank;
 	private boolean running = true;
-
+	/** Constructor demands a reference to the "controller" **/ //which allows the class to work with other classes
 	public Scoreboard(Bank bank) {
 		this.bank = bank;
 	}
@@ -30,10 +30,11 @@ public class Scoreboard implements Runnable {
 				Thread.sleep(400);
 			}
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	//Setters and Getters
 	
 	public void stopThread(){
 		running = false;
